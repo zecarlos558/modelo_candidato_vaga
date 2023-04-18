32cc0112cea387fbf48c93e81f817cc305fef9f6
José Carlos Nunes Coelho Neto <45573159+zecarlos558@users.noreply.github.com>
2023-04-18 14:49:24 -0300

    Update main.blade.php

--- cadastro-candidatos-vagas/resources/views/components/main.blade.php
+++ cadastro-candidatos-vagas/resources/views/components/main.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.layout')
 
 @section('content')
-    <div {{ $attributes->merge(['class' => '']) }} id="{{ $id ? $id : 'tech-container' }}">
+    <div {{ $attributes->merge(['class' => '']) }} id="{{ @$id ? $id : 'tech-container' }}">
         <x-erros-request></x-erros-request>
         <x-confirm-request></x-confirm-request>
         {{ $slot }}
